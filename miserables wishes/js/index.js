2bd5db5b72182fe11815efafa9e92fc9b1c12652
RedAnil <159431851+RedAnil@users.noreply.github.com>
2024-10-24 15:57:13 +0200

    Update index.js
    Added an "Easier" way to calculate wishes.
    Removed loops, added calculations.

--- miserables wishes/js/index.js
+++ miserables wishes/js/index.js
@@ -22,21 +22,17 @@ class Calculator {
 
 
     calcuateWishesForKnowledge(value) {
-        this.knowledge = Number(value)
-        this.wishes = 0
-        for (let fullKnowledgeCost = (15 + (this.offset* 5 )); fullKnowledgeCost <= this.knowledge; fullKnowledgeCost += (15 + (5 * (this.wishes + this.offset)))) {
-            this.wishes++
-        }
+        this.knowledge = Number(value) - (this.offset * 15 + 5 * ((this.offset-1) * (this.offset/2)))
+        // w = (-29 + sqrt(841 + (8k))) / 2
+        this.wishes = Math.floor((-29 + Math.sqrt(841 + (8*this.knowledge))) / 2);
+
         document.querySelector("#wishes").value = this.wishes;
     }
 
     calculateKnowledgeForWishes(value) {
-        this.wishes = Number(value);
-        this.knowledge = 0;
-        for (let wishAmount = 1 + this.offset; wishAmount <= (this.wishes + this.offset); wishAmount++) {
-            console.log(wishAmount)
-            this.knowledge += (15 + (5 * wishAmount));
-        }
+        this.wishes = Number(value) + this.offset;
+        // k = w(15) + w((w-1)/2)
+        this.knowledge = this.wishes * 15 + 5 * ((this.wishes-1) * (this.wishes/2))
         document.querySelector("#knowledge").value = this.knowledge;
     }
 
@@ -44,7 +40,7 @@ class Calculator {
     changeOffset(value) {
         this.offset = Number(value)
         if (this.wishes != 0 || document.querySelector("#wishes").value != "" || document.querySelector("#wishes").value != null) {
-            this.wishes = document.querySelector("#wishes").value;
+            this.wishes = Number(document.querySelector("#wishes").value);
             this.calculateKnowledgeForWishes(this.wishes);
         } else {
             if (this.knowledge != 0 || document.querySelector("#knowledge").value != "" || document.querySelector("#knowledge").value != null) {
